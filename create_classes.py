7a344cf84f9e34d0da15f070f2a360a014ebfe68
JayTongue <jctung2@illinois.edu>
2023-11-05 16:45:34 -0600

    Created superclass Insect and subclasses wasp and SLF

--- create_classes.py
+++ create_classes.py
@@ -23,27 +23,74 @@ class City(Location):
 
 
 class Vehicle:
-    def __init__(self, size):
-        self.size = size
+    def __init__(self):
 
 
 class Truck(Vehicle):
-    def __init__(self, size, infection_prob):
-        super().__init__(self, size)
+    def __init__(self, infection_prob):
+        super().__init__(self)
         self.avg_dist_per_day = float(random.normal(loc=500, scale=1)) # in miles
         self.infection_prob = infection_prob
 
-    def make_trip(self, size, infection_prob):
+    def make_trip(self, infection_prob):
         infect_rand = float(random.normal(loc=0, scale=1))
-        if infect_rand >= 0:
-            self.infection_prob = size * infect_rand * 0.001
+        if infect_rand > 0:
+            self.infection_prob = infect_rand * 0.01
         else:
             self.infection_prob = 0
         return infection_prob
 
 
 class Train(Vehicle):
-    def __init__(self, size):
-        super().__init__(self, size)
-        self.infection_prob = 0.2  # placeholder likelihood of infection.
+    def __init__(self, infection_prob):
+        super().__init__(self)
         self.avg_dist_per_day = float(random.normal(loc=600, scale=1))  # in miles
+        self.infection_prob = infection_prob
+
+    def make_trip(self, infection_prob):
+        infect_rand = float(random.normal(loc=0, scale=1))
+        if infect_rand > 0:
+            self.infection_prob = infect_rand * 0.01
+        else:
+            self.infection_prob = 0
+        return infection_prob
+
+
+class Insect:
+    def __init__(self, age, hunger):
+        self.age = age
+        self.hunger = hunger
+
+
+class SLF(Insect):
+    def __init__(self, age, hunger):
+        super().__init__(self, age, hunger)
+
+    def seek_food(self, hunger):
+        if hunger >= 10:
+            # Prioritize heading towards areas with trees over laying eggs
+
+
+    def propagate(self, age):
+        if age >= 10:
+            #prioritize creating more SLF
+
+    def old_age(self, age):
+        if age > 8:
+            # delete instance of SLF class
+
+
+class Wasp(Insect):
+    def __init__(self, age, hunger):
+        super().__init__(self, age, hunger)
+
+
+    def hunt(self, hunger):
+        if hunger >= 10:
+            # Prioritize hunting SLF
+        for  # SLF killed
+            # create another wasp instance
+
+    def old_age(self, age):
+        if age > 8:
+            # delete instance of wasp class
