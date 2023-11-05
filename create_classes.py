271f7c822d0f0df3d6b8825eb6361dd8f7449d7e
JayTongue <jctung2@illinois.edu>
2023-11-05 13:28:00 -0600

    Added Vehicle superclass and truck and train subclasses

--- create_classes.py
+++ create_classes.py
@@ -5,8 +5,31 @@ This file will contain the classes to be used in the MC simualtion
 """
 
 
-class City:
-    def __init__(self, population, lat, lng):
-        self.population = population
+class Location:
+    def __init__(self, lat, lng, ToH_density, OT_density):
         self.lat = lat
         self.lng = lng
+        self.ToH_density = ToH_density  # "tree of heaven density"
+        self.OT_density = OT_density  # "Other trees density"
+
+
+class City(Location):
+    def __init__(self, lat, lng, ToH_density, OT_density, population):
+        super().__init__(lat, lng, ToH_density, OT_density)
+        self.population = population
+
+
+class Vehicle:
+    def __init__(self, size, commercial):
+        self.size = size
+        self.commercial = commercial
+
+
+class Truck(Vehicle):
+    def __init__(self):
+        pass
+
+
+class Train(Vehicle):
+    def __init__(self):
+        pass
